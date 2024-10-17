a4af8609a8490fc2552a1d8cc1a50728cb4b9aa0
Bhiman Singha <mbhiman70@gmail.com>
2024-10-17 13:28:30 +0530

    Created a post API to find user by email

--- src/app.js
+++ src/app.js
@@ -16,9 +16,8 @@ mongoDB()
 
 app.use(express.json());
 
-app.get("/sign-up", async (req, res) => {
+app.post("/sign-up", async (req, res) => {
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("User added succeddfully.");
@@ -33,7 +32,7 @@ app.delete("/delete-user", async (req, res) => {
     await User.findByIdAndDelete({ _id });
     console.log("User deleted successfully");
   } catch (error) {
-    error.status(404).send("Something went wrong", error);
+    res.status(404).send("Something went wrong"+ error.message);
   }
 });
 
@@ -43,7 +42,20 @@ app.patch("/update-user", async (req, res) => {
     const data = req.body;
     await User.findByIdAndUpdate({ _id: userId }, data);
   } catch (error) {
-    res.status(404).send("Something went wrong");
+    res.status(404).send("Something went wrong:" + error.message);
   }
 });
+
+app.post("/find-user-by-email" , async (req, res) =>{
+  const userEmail = req.body.email;
+  try {
+    const user = await User.findOne({email : userEmail});
+    if(!user){
+      return res.status(404).send("User not found");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send("Error: " + error.message);
+  }
+})
 
