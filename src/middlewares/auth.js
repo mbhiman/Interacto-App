e23296ad904f3dfa4a024e3ffecc666b08eb738e
Bhiman Singha <mbhiman70@gmail.com>
2024-10-20 21:16:15 +0530

    Created userAuth middleware to check authendicity using web token

---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const User = require("../model/user");
+
+const userAuth = async (req, res, next) => {
+    try {
+        const { token } = req.cookies;
+        
+        if(!token) {
+            throw new Error("Invalid Token");    
+        }
+
+        const decodedObj = await jwt.verify(token, "DevTinder@2001");
+        const { _id } = decodedObj;
+        const user = await User.findById(_id);
+        if(!user) {
+            throw new Error("User not found!");   
+        }
+        next();
+
+    } catch (error) { 
+        res.status(404).send("Something went wrong" + error.message);
+    }
+};
+
+module.exports = { userAuth };
