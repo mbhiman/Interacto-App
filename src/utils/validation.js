d9e40b989682af7d0a4d236bd9045c40f4e8eaf9
Bhiman Singha <mbhiman70@gmail.com>
2024-10-24 19:48:06 +0530

    Added validation of edit fields of profile data

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,24 +2,40 @@ const validator = require('validator');
 
 const validateSignupData = (req) => {
 
-    const {firstName, lastName, email, password} = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
-    if(!firstName || !lastName){
+    if (!firstName || !lastName) {
         throw new Error("Enter a valid name");
 
-    } else if( firstName.minLength <3 || firstName.maxlength >30){
+    } else if (firstName.minLength < 3 || firstName.maxlength > 30) {
         throw new Error("First name should be within 3-30 words");
 
-    } else if(lastName.minLength <3 || lastName.maxlength >30){
+    } else if (lastName.minLength < 3 || lastName.maxlength > 30) {
         throw new Error("Last name should be within 3-30 words");
-        
-    } else if(!validator.isEmail(email)){
+
+    } else if (!validator.isEmail(email)) {
         throw new Error("Enter a valid email");
-        
-    } else if(!validator.isStrongPassword(password)){
+
+    } else if (!validator.isStrongPassword(password)) {
         throw new Error("Enter a strong password");
-        
+
     }
 }
 
-module.exports = { validateSignupData};
+const validateEditProfileData = (req) => {
+    const allowedEditFields = [
+        "firstName", 
+        "lastName", 
+        "email", 
+        "about", 
+        "age", 
+        "gender", 
+        "skills"
+    ];
+    const isEditAllowed = Object.keys(req.body).every(fields => {
+        allowedEditFields.includes(fields)
+    });
+    return isEditAllowed;
+}
+
+module.exports = { validateSignupData, validateEditProfileData };
